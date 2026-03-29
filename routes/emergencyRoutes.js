467c64f2519a47659cd33d8f683a72ab80fcaa14
sumit-kade-008 <sumitkade098@gmail.com>
2026-03-29 21:55:27 +0530

    Delete routes directory

--- routes/emergencyRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createEmergencyRequest, getEmergencyRequests } = require('../controllers/emergencyController');
-const { protect } = require('../middleware/auth');
-
-router.post('/', protect, createEmergencyRequest);
-router.get('/', getEmergencyRequests); 
-
-module.exports = router;
